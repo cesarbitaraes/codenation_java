253e6fc8c5046a57d2a31171d21357546cb14822
César Bitarães <caabitaraes@gmail.com>
2020-04-21 15:16:49 -0300

    Desafio_02_Criptografia

---
+++ java-6/src/main/java/challenge/CriptografiaCesariana.java
@@ -0,0 +1,32 @@
+package challenge;
+
+public class CriptografiaCesariana implements Criptografia {
+
+    @Override
+    public String criptografar(String texto) {
+        String criptografada = "";
+        for(int i=0;i<texto.length();i++){
+            if (((texto.charAt(i) >= 'A') && (texto.charAt(i) <= 'Z')) ||
+                    ((texto.charAt(i) >= 'a') && (texto.charAt(i) <= 'z'))) {
+                criptografada = criptografada + (char)(texto.charAt(i) + 3);
+            }else{
+                criptografada = criptografada + (texto.charAt(i));
+            }
+        }
+        return criptografada.toLowerCase();
+    }
+
+    @Override
+    public String descriptografar(String texto) {
+        String descriptografada = "";
+        for(int i=0;i<texto.length();i++){
+            if (((texto.charAt(i) >= 'A') && (texto.charAt(i) <= 'Z')) ||
+                    ((texto.charAt(i) >= 'a') && (texto.charAt(i) <= 'z'))) {
+                descriptografada = descriptografada + (char)(texto.charAt(i) - 3);
+            }else{
+                descriptografada = descriptografada + (texto.charAt(i));
+            }
+        }
+        return descriptografada.toLowerCase();
+    }
+}
