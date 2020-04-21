11325c2aec15577bc6d3197cd3c63890214d2ee5
César Bitarães <caabitaraes@gmail.com>
2020-04-21 16:01:52 -0300

    Desafio_02_Criptografia_Fixed_01

--- java-6/src/main/java/challenge/CriptografiaCesariana.java
+++ java-6/src/main/java/challenge/CriptografiaCesariana.java
@@ -4,7 +4,10 @@ public class CriptografiaCesariana implements Criptografia {
 
     @Override
     public String criptografar(String texto) {
-        String criptografada = "";
+        if (texto.isEmpty()) {
+            throw new IllegalArgumentException("O texto deve conter pelo menos 1 caracter.");
+        }
+		String criptografada = "";
         for(int i=0;i<texto.length();i++){
             if (((texto.charAt(i) >= 'A') && (texto.charAt(i) <= 'Z')) ||
                     ((texto.charAt(i) >= 'a') && (texto.charAt(i) <= 'z'))) {
@@ -18,7 +21,10 @@ public class CriptografiaCesariana implements Criptografia {
 
     @Override
     public String descriptografar(String texto) {
-        String descriptografada = "";
+        if (texto.isEmpty()) {
+            throw new IllegalArgumentException("O texto deve conter pelo menos 1 caracter.");
+        }
+		String descriptografada = "";
         for(int i=0;i<texto.length();i++){
             if (((texto.charAt(i) >= 'A') && (texto.charAt(i) <= 'Z')) ||
                     ((texto.charAt(i) >= 'a') && (texto.charAt(i) <= 'z'))) {
